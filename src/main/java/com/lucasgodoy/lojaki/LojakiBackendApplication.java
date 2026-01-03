259312de65b9a2c039a968d696bc3d76360b4dee
lucasgodoyy <ehlucasgodoy10@gmail.com>
2026-01-03 15:43:14 +1100

    chore: initial project structure with packages and entity skeletons

---
+++ src/main/java/com/lucasgodoy/lojaki/LojakiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lucasgodoy.lojaki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojakiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LojakiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucasgodoy/lojaki/LojakiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasgodoy.lojaki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojakiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
